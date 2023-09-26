51edb140542ff4cd6bdb10d72e84d7b861d13832
piwriw <piwriw@163.com>
2023-09-26 20:04:55 +0800

    [doc]:add cast package example,类型转换
    Signed-off-by: piwriw <piwriw@163.com>

---
+++ cast/cast.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cast"
+)
+
+/*
+go get github.com/spf13/cast
+这是一个类型转换库
+*/
+func main() {
+	// ToString
+	fmt.Println(cast.ToString("leedarjun"))        // leedarjun
+	fmt.Println(cast.ToString(8))                  // 8
+	fmt.Println(cast.ToString(8.31))               // 8.31
+	fmt.Println(cast.ToString([]byte("one time"))) // one time
+	fmt.Println(cast.ToString(nil))                // ""
+
+	var foo interface{} = "one more time"
+	fmt.Println(cast.ToString(foo)) // one more time
+
+	// ToInt
+	fmt.Println(cast.ToInt(8))     // 8
+	fmt.Println(cast.ToInt(8.31))  // 8
+	fmt.Println(cast.ToInt("8"))   // 8
+	fmt.Println(cast.ToInt(true))  // 1
+	fmt.Println(cast.ToInt(false)) // 0
+
+	var eight interface{} = 8
+	fmt.Println(cast.ToInt(eight)) // 8
+	fmt.Println(cast.ToInt(nil))   // 0
+}
