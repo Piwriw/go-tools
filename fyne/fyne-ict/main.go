7d0694f16ede81bba62aa2467938dd6c308de210
piwriw <piwriw@163.com>
2023-09-29 16:35:22 +0800

    [fix]:fix some commnets
    Signed-off-by: piwriw <piwriw@163.com>

--- fyne/fyne-ict/main.go
+++ fyne/fyne-ict/main.go
@@ -14,10 +14,15 @@ GUI中文文档 https://go-circle.cn/fyne-press/v1.0/1-getting-started/introduct
 1. 开启GO111MODULE=on
 2. 设置中国代理 export GOPROXY=https://goproxy.cn
 3. 下载依赖 go mod tidy
+
 考核任务：
 1. 难度：Easy 读取score.xlsx文件,过滤出 开课学期 课程代码 成绩 课程名称 考核方式 课程性质 课程属性 并且赋值给rows
-2. 难度：基本要求Easy  进阶要求：Middle 通过获得的考试成绩数据  计算机该同学 大学平均四年绩点
+2. 难度：基本要求：Easy  进阶要求：Middle 通过获得的考试成绩数据  计算该同学 大学平均四年绩点
 3. 难度：Middle 绘制出出这个同学大学四年的课程属性的饼状图占比情况
+
+Tips:
+如果你会使用Excel函数，你可以先直接操作Excel来验证你的程序输出结果
+
 */
 /*
 printGraph  绘制出出这个同学大学四年的课程属性的饼状图占比情况
