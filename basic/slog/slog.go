945076e285c25fea260702db977092d01f140a58
joohwan <zhouhuan.chen@yunqutech.com>
2024-07-23 15:40:09 +0800

    example:update slog example

--- basic/slog/slog.go
+++ basic/slog/slog.go
@@ -7,6 +7,24 @@ import (
 
 func main() {
 	name := "sss"
+	slog.Info("This is info log")
+	slog.Warn("This is warning log")
+	slog.Error("This is error log")
+
+	// Group 日志
+	g1 := slog.Group("g1", slog.String("module", "authentication"), slog.String("method", "login"))
+	g2 := slog.Group("g2", slog.String("module", "authentication"), slog.String("method", "login"))
+
+	slog.Info("User login attempt",
+		slog.String("service", "login-service"),
+		g1,
+	)
+	slog.Info("User login attempt",
+		slog.String("service", "login-service"),
+		g2,
+	)
+
+	slog.Info("msg", slog.String("name", name))
 	slog.Error("ERROR: value is empty", slog.Any("name", name))
 	// 设置日志格式为JSON
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{})))
