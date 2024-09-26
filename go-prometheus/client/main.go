f2737363acc08edfba0e8226d85114fe2e83e408
joohwan <zhouhuan.chen@yunqutech.com>
2024-09-26 10:32:34 +0800

    example:update go-prometheus

---
+++ go-prometheus/client/main.go
@@ -0,0 +1,95 @@
+package client
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/prometheus/prometheus/promql/parser"
+
+	"github.com/pkg/errors"
+
+	"github.com/prometheus/prometheus/promql"
+	"github.com/prometheus/prometheus/util/teststorage"
+
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
+)
+
+// PrometheusClient 封装 Prometheus API 客户端
+type PrometheusClient struct {
+	client v1.API
+}
+
+// NewPrometheusClient 初始化 PrometheusClient
+func NewPrometheusClient(address string) (*PrometheusClient, error) {
+	client, err := api.NewClient(api.Config{
+		Address: address,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &PrometheusClient{
+		client: v1.NewAPI(client),
+	}, nil
+}
+
+// Query 执行 Prometheus 查询
+func (p *PrometheusClient) Query(query string, ts time.Time) (model.Value, error) {
+	result, warnings, err := p.client.Query(context.Background(), query, ts)
+	if err != nil {
+		return nil, err
+	}
+	if len(warnings) > 0 {
+		slog.Warn("PrometheusClient GET Warnings INFO", slog.Any("warnings", warnings))
+	}
+	return result, nil
+}
+
+// QueryRange 执行 Prometheus 时间范围查询
+func (p *PrometheusClient) QueryRange(query string, start, end time.Time, step time.Duration) (model.Value, error) {
+	r := v1.Range{
+		Start: start,
+		End:   end,
+		Step:  step,
+	}
+	result, warnings, err := p.client.QueryRange(context.Background(), query, r)
+	if err != nil {
+		return nil, err
+	}
+	if len(warnings) > 0 {
+		slog.Warn("PrometheusClient GET Warnings INFO", slog.Any("warnings", warnings))
+	}
+	return result, nil
+}
+
+func (p *PrometheusClient) Validate(querySQL string) error {
+
+	// 创建查询引擎
+	opts := promql.EngineOpts{
+		MaxSamples:    50000000, // 设置查询的最大样本
+		Timeout:       0,        // 超时时间
+		Logger:        nil,      // 可以传递 logger
+		LookbackDelta: 0,
+	}
+
+	engine := promql.NewEngine(opts)
+
+	// 使用 Prometheus 的测试存储来执行查询（这里不真正评估数据，只做语法校验）
+	storage := teststorage.New(nil)
+	defer storage.Close()
+	_, err := engine.NewInstantQuery(context.TODO(), storage, nil, querySQL, time.Now())
+	if err != nil {
+		return errors.Wrapf(err, "Valide query is failed")
+	}
+	return nil
+}
+func (p *PrometheusClient) Prettify(querySQL string) (string, error) {
+	expr, err := parser.ParseExpr(querySQL)
+	if err != nil {
+		return nil, err
+	}
+	return parser.Prettify(expr), nil
+}
---
+++ go-prometheus/client/main_test.go
@@ -0,0 +1,54 @@
+package client
+
+import (
+	"log/slog"
+	"testing"
+	"time"
+
+	"github.com/prometheus/prometheus/promql/parser"
+)
+
+func TestQuery(t *testing.T) {
+	client, err := NewPrometheusClient("http://10.0.0.195:9002")
+	if err != nil {
+		t.Fatal(err)
+	}
+	query, err := client.Query("is_alive{category=\"database.gaussdb\"}", time.Now())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(query)
+
+}
+
+func TestQueryRange(t *testing.T) {
+	active := new(bool)
+	t.Logf("%v", *active)
+}
+
+func TestValidted(t *testing.T) {
+	// 要校验的 PromQL 查询
+	query := "count by(instance, name,ip)(db_lock{W_WAITER=~\".+\"})"
+	client, err := NewPrometheusClient("http://10.0.0.195:9002")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := client.Validate(query); err != nil {
+		t.Fatal(err)
+	}
+	t.Log("PASS")
+}
+
+func TestName(t *testing.T) {
+
+}
+
+func TestPrettify(t *testing.T) {
+	expr, err := parser.ParseExpr("a @ 1")
+	if err != nil {
+		slog.Error("ParseExpr is failed", slog.Any("err", err))
+	}
+	t.Log(parser.Prettify(expr))
+
+	parser.ParseMetric(s.Labels)
+}
