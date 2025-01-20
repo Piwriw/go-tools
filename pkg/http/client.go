24dd915b98baa096a6593d4fe606acf2e6fe0e66
joohwan <piwriw@163.com>
2025-01-20 20:10:34 +0800

    wip:http func

--- pkg/http/client.go
+++ pkg/http/client.go
@@ -3,8 +3,11 @@ package http
 import (
 	"bytes"
 	"io"
+	"mime/multipart"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -70,4 +73,59 @@ func (h *Client) Post(url string, data []byte) ([]byte, error) {
 		return nil, err
 	}
 	return body, nil
+}
+
+// PostFile sends a file in a POST request using multipart/form-data
+func (h *Client) PostFile(url, fieldName, filePath string) ([]byte, error) {
+	// 创建一个缓冲区和 multipart writer
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	// 打开文件
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// 创建文件字段
+	part, err := writer.CreateFormFile(fieldName, filepath.Base(filePath))
+	if err != nil {
+		return nil, err
+	}
+
+	// 将文件内容写入 multipart
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return nil, err
+	}
+
+	// 关闭 multipart writer 以完成请求体
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	// 创建 HTTP 请求
+	request, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	// 设置 Content-Type 为 multipart/form-data
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// 发送请求
+	res, err := h.client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	// 读取响应
+	responseBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return responseBody, nil
 }
