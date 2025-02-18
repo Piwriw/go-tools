91b067799a7796f9e1313ce38c805c9cff905285
joohwan <piwriw@163.com>
2025-02-18 16:07:33 +0800

    feat:add some empty func
    Signed-off-by: joohwan <piwriw@163.com>

--- pkg/chrono/job.go
+++ pkg/chrono/job.go
@@ -18,12 +18,19 @@ var (
 		slog.Info("BeforeJobRunsAfterFuncErrors", "jobID", jobID, "jobName", jobName)
 	}
 	defaultAfterJobRunsWithError = func(jobID uuid.UUID, jobName string, err error) {
-		slog.Info("AfterJobRunsWithError", "jobID", jobID, "jobName", jobName)
+		slog.Error("AfterJobRunsWithError", "jobID", jobID, "jobName", jobName, "err", err)
 	}
 	defaultAfterJobRunsWithPanic = func(jobID uuid.UUID, jobName string, recoverData any) {
-		slog.Info("AfterJobRunsWithPanic", "jobID", jobID, "jobName", jobName)
+		slog.Error("AfterJobRunsWithPanic", "jobID", jobID, "jobName", jobName, "recoverData", recoverData)
 	}
 	defaultAfterLockError = func(jobID uuid.UUID, jobName string, err error) {
-		slog.Info("AfterLockError", "jobID", jobID, "jobName", jobName)
+		slog.Error("AfterLockError", "jobID", jobID, "jobName", jobName, "err", err)
+	}
+	EmptyWatchFunc             = func(event MonitorJobSpec) {}
+	EmptyAfterJobRunsWithError = func(jobID uuid.UUID, jobName string, err error) {
+		slog.Error("AfterJobRunsWithError", "jobID", jobID, "jobName", jobName, "err", err)
+	}
+	EmptyAfterJobRunsWithPanic = func(jobID uuid.UUID, jobName string, recoverData any) {
+		slog.Error("AfterJobRunsWithPanic", "jobID", jobID, "jobName", jobName, "recoverData", recoverData)
 	}
 )
