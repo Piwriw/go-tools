193b22cdb25f95b3265655c70e787d821f1c4f92
joohwan <piwriw@163.com>
2025-03-05 16:38:15 +0800

    fix：fix alertmanager Format
    Signed-off-by: joohwan <piwriw@163.com>

--- pkg/alertmanager/common.go
+++ pkg/alertmanager/common.go
@@ -6,7 +6,7 @@ const (
 	AlertStatusFiring   = "活跃"
 	AlertStatusResolved = "恢复"
 )
-const AgilexFormat = "[%sAgilex告警信息] \n类型：%s \n实例名称：%s \nIP：%s \n告警名称：%s \n告警级别：%s \n告警详情：%s \n开始时间：%s"
+const AlertFormat = "[%sAlert告警信息] \n类型：%s \n实例名称：%s \nIP：%s \n告警名称：%s \n告警级别：%s \n告警详情：%s \n开始时间：%s"
 
 var emailTableTemplate = `
 <div class="table_content">
