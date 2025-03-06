6943635532a66f464a7a4a64d2539ea9b9edd404
joohwan <piwriw@163.com>
2025-03-06 16:52:28 +0800

    feat:add time format func
    Signed-off-by: joohwan <piwriw@163.com>

--- pkg/format/time.go
+++ pkg/format/time.go
@@ -1,6 +1,36 @@
 package format
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+// CSTTime 返回当前北京时间（CST）
+func CSTTime() (time.Time, error) {
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		return time.Now(), err
+	}
+	return time.Now().In(loc), nil
+}
+
+// TimeZone 返回指定时区的当前时间
+func TimeZone(timezone string) (time.Time, error) {
+	// 默认时区
+	locName := "Asia/Shanghai"
+	if timezone != "" {
+		locName = timezone
+	}
+
+	// 加载时区
+	loc, err := time.LoadLocation(locName)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("failed to load location %s: %v", locName, err)
+	}
+
+	// 返回当前时间（带时区信息）
+	return time.Now().In(loc), nil
+}
 
 // ConvertToWeekdays 将[]int转换为[]time.Weekday
 func ConvertToWeekdays(intArray []int) []time.Weekday {
